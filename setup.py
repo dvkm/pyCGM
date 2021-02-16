5e35c23738ff8799c887bd4fa6d696df190647bf
David Kim <d@vid.kim>
2021-02-15 21:50:48 -0500

    use updated c3d

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=2.7, !=3.0, !=3.1, !=3.2, !=3.3, !=3.4, !=3.5',
-    install_requires=['numpy>=1.15','scipy'],
+    install_requires=['numpy>=1.15','scipy','c3d'],
     package_data={
         "": [
             "../SampleData/*/*.c3d",
